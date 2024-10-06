ea86271b2cfb1b7bf677c75de10b8919cff1fa9a
saltie2193 <saltie@protonmail.com>
2024-10-06 16:44:52 +0200

    Update `create_random_user` and `create_random_item` to enable overrides of all available parameters.

--- backend/app/tests/utils/user.py
+++ backend/app/tests/utils/user.py
@@ -1,8 +1,13 @@
+from collections.abc import Generator
+from contextlib import contextmanager
+from typing import Protocol
+
 from fastapi.testclient import TestClient
-from sqlmodel import Session
+from sqlmodel import Session, delete
 
 from app import crud
 from app.core.config import settings
+from app.core.security import get_password_hash
 from app.models import User, UserCreate, UserUpdate
 from app.tests.utils.utils import random_email, random_lower_string
 
@@ -13,18 +18,111 @@ def user_authentication_headers(
     data = {"username": email, "password": password}
 
     r = client.post(f"{settings.API_V1_STR}/login/access-token", data=data)
+    assert r.status_code == 200
     response = r.json()
     auth_token = response["access_token"]
     headers = {"Authorization": f"Bearer {auth_token}"}
     return headers
 
 
-def create_random_user(db: Session) -> User:
-    email = random_email()
-    password = random_lower_string()
-    user_in = UserCreate(email=email, password=password)
-    user = crud.create_user(session=db, user_create=user_in)
+def create_random_user(
+    db: Session,
+    email: str | None = None,
+    password: str | None = None,
+    full_name: str | None = None,
+    is_active: bool | None = None,
+    is_superuser: bool | None = None,
+    *,
+    hash_password: bool = True,
+    commit: bool = True,
+) -> User:
+    """
+    Create a random user in the given database session.
+
+    Provided parameters will overwrite random values.
+    By default, password will not be hashed to increase performance of tests.
+
+    :param db: Database session to add the user to.
+    :param email: Overwrite ``username``.
+    :param password: Overwrite ``password``.
+    :param full_name: Overwrite ``full_name``
+    :param is_active: Overwrite ``is_active``.
+    :param is_superuser: Overwrite ``is_superuser``.
+    :param hash_password: Whether to hash the password or not. (default ``True``)
+    :param commit: Whether to commit the transaction to the database. (default: ``True``)
+    :return: Created user.
+    """
+    if email is None:
+        email = random_email()
+    if password is None:
+        password = random_lower_string()
+    password = get_password_hash(password) if hash_password else password
+    user = User(
+        email=email,
+        hashed_password=password,
+        full_name=full_name,
+        is_active=is_active,
+        is_superuser=is_superuser,
+    )
+    db.add(user)
+    if commit:
+        db.commit()
+        db.refresh(user)
     return user
+
+
+class CreateUserProtocol(Protocol):
+    def __call__(
+        self,
+        email: str | None = ...,
+        password: str | None = ...,
+        full_name: str | None = ...,
+        is_active: bool | None = ...,
+        is_superuser: bool | None = ...,
+        *,
+        hash_password: bool = ...,
+        commit: bool = ...,
+        cleanup: bool = ...,
+    ) -> User:
+        ...
+
+
+@contextmanager
+def create_user_context(db: Session) -> Generator[CreateUserProtocol, None, None]:
+    """
+    Context Manager to return a user factory that can be used to create users in the given database.
+    """
+    created = []
+
+    def factory(
+        email: str | None = None,
+        password: str | None = None,
+        full_name: str | None = None,
+        is_active: bool | None = None,
+        is_superuser: bool | None = None,
+        *,
+        hash_password: bool = True,
+        commit: bool = True,
+        cleanup: bool = True,
+    ) -> User:
+        user = create_random_user(
+            db=db,
+            email=email,
+            password=password,
+            full_name=full_name,
+            is_active=is_active,
+            is_superuser=is_superuser,
+            hash_password=hash_password,
+            commit=commit,
+        )
+        if cleanup:
+            created.append(user)
+        return user
+
+    yield factory
+    # cleanup
+    stmt = delete(User).where(User.id.in_(u.id for u in created))
+    db.execute(stmt)
 
 
 def authentication_token_from_email(
